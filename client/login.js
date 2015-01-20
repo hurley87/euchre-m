94a54e02bcdeebf4baedcfe54f4863308189eb6a
david hurley <dhurls99@gmail.com>
2015-01-19 22:19:54 -0500

    inital commit

---
+++ client/login.js
@@ -0,0 +1,29 @@
+Template.signup.events({
+	'click button': function(evt, template) {
+		evt.preventDefault();
+
+		Accounts.createUser({
+			email: template.find('#su-email').value,
+			username: template.find('#su-username').value,
+			password: template.find('#su-password').value
+		});
+	}
+});
+
+Template.login.events({
+	'click button': function(evt, template) {
+		evt.preventDefault();
+
+		Meteor.loginWithPassword(
+			template.find('#si-email').value,
+			template.find('#si-password').value
+		);
+	}
+});
+
+Template.logout.events({
+	'click button': function(evt, tempkate) {
+		evt.preventDefault();
+		Meteor.logout();
+	}
+});
