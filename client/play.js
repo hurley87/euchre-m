f79a1d688b54c996bffeae937665830daf8d375f
david hurley <dhurls99@gmail.com>
2015-02-19 17:11:30 -0500

    bug fix on play view

--- client/play.js
+++ client/play.js
@@ -1,8 +1,5 @@
 Template.gameStart.events({
 	'click .pickcard': function (evt, template) {
-        // Meteor.call('createGame', template.data._id);
-
-
         var game = Games.findOne({ _id: template.data._id });
         game.trump = template.data.decisionCard.suit;
 
