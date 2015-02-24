df184a98ca616b9fc0ac27290196aa1c96bfc663
david hurley <dhurls99@gmail.com>
2015-02-24 17:27:18 -0500

    using picking up card

--- client/play.js
+++ client/play.js
@@ -1,11 +1,24 @@
 Template.gameStart.events({
 	'click .pickcard': function (evt, template) {
 		evt.preventDefault();
-        var game = Games.findOne({ _id: template.data._id });
-        var newTrump = template.data.decisionCard.suit;
+        $(evt.currentTarget).parent().siblings('.card').addClass('chooseCardToDrop');
+        $('.decisionTime').html('<p>Choose a card to drop please.</p>')
+       	
+    },
+    'click .chooseCardToDrop': function(evt, template) {
+    	evt.preventDefault();
+    	var choosenCard = this;
+    	var game = Games.findOne({ _id: template.data._id });
+    	var userHand = template.data.user.hand;
+    	var indexOfCard = _.indexOf(userHand, choosenCard);
+    	var newTrump = template.data.decisionCard.suit;
 
-       	Games.update(game._id, { $set: { trump : newTrump, currentTurn: game.currentTurn.reverse() }  } );
+    	userHand[indexOfCard] = template.data.decisionCard;
+ 		// set userHand to contain new card
+ 		
+    	Games.update(game._id, { $set: { trump : newTrump, currentTurn: game.currentTurn.reverse()}  } );
     }
+    
 });	
 
 Template.gameStart.helpers({
@@ -24,3 +37,6 @@ Template.status.events({
 });
 
 
+Template.myHand.events({
+	
+});
